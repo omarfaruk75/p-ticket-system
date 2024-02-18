ea8bc9c60fb40012ccbf31dd6092199f7d4c32a4
omar faruk <farukomarcpa@gmail.com>
2024-02-18 18:51:33 +0600

    dibabled function added

--- js/index.js
+++ js/index.js
@@ -5,22 +5,25 @@
     let seatPrice = 0;
     let seatPerCount = 0; 
     let grandPrice = 0 ;
+    let clickedButtonCount = 0;
     let coupleDiscount;   
     let coupanDiscount;   
     
 
     for(const seat of seatBtn){
       seat.addEventListener('click', function(e){
-        count = parseInt(count - 1);
-        seatPrice = parseInt(seatPrice + 500);
-        grandPrice = parseInt(seatPrice * 0.2);
-        seatPerCount = parseInt(seatPerCount + 1);
-        const seatName = e.target.innerText;
-        const seatCategory = 'Economy';
-        const seatPerPrice = 500;
-        const selectTargetName = document.getElementById('seat-name');
-        const selectTargetClass = document.getElementById('seat-class');
-        const selectTargetPrice = document.getElementById('price-per-price');
+      count = parseInt(count - 1);
+      seatPrice = parseInt(seatPrice + 500);
+      seatPerCount = parseInt(seatPerCount + 1);
+      const seatName = e.target.innerText;
+      e.target.style.backgroundColor = 'green';
+      e.target.setAttribute('disabled',true);
+      const seatCategory = 'Economy';
+      const seatPerPrice = 500;
+      const selectTargetName = document.getElementById('seat-name');
+      const selectTargetClass = document.getElementById('seat-class');
+      const selectTargetPrice = document.getElementById('price-per-price');
+
        const p1 =document.createElement('p');
        p1.innerText = seatName;
        selectTargetName.appendChild(p1);
@@ -30,12 +33,7 @@
        const p3 =document.createElement('p');
        p3.innerText = seatPerPrice;
        selectTargetPrice.appendChild(p3);
-       if(seatPrice === 2000){{
-        alert('four ticket purchsae');
-        return;
-       }
-
-       }
+      
 
         selectedSeats('total-seat', count);
         selectedSeats('seat-price', seatPrice);
@@ -46,6 +44,17 @@
             alert('There is no available seat')
             return;
         }
+          if (seatPrice >= 2000 || clickedButtonCount >= 4) {
+          alert('You have reached the maximum limit for this transaction');
+            for (const seat of seatBtn) {
+           if (seat !== e.target) {
+             seat.disabled = true;
+          }
+      }
+
+      return;
+    }
+
 
       })
     }
